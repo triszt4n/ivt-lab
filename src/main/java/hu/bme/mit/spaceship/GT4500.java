2bfcc1a422a032892b3bcc43b0d98dd3d0684cf5
triszt4n <piller.trisztan@simonyi.bme.hu>
2022-04-08 08:55:06 +0200

    Add silly comment

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -16,7 +16,7 @@ public class GT4500 implements SpaceShip {
   }
 
   public boolean fireLaser(FiringMode firingMode) {
-    // TODO not implemented yet
+    // TODO not implemented yet HAHAHAHAHAHA
     return false;
   }
 
